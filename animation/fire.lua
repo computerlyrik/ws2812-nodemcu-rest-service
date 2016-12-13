d7cb24e8d4c01303fd8c735328cf8f4a6f04f19e
Christian Fischer <sw-dev@computerlyrik.de>
2016-12-13 19:38:49 +0100

    update fire

--- animation/fire.lua
+++ animation/fire.lua
@@ -9,42 +9,38 @@ return function (rgbdata, speed)
   local sparking = 120
   local numleds = buffer:size()
 
-
-  animationData = {}
   animationData.heat = {}
   for i=1, numleds do
     animationData.heat[i] = 0
   end
 
   tmr.alarm(0, speed, tmr.ALARM_AUTO, function()
-
-    local cooldown = math.random(0, ((cooling * 10) / buffer:size() ) + 2);
-
+    print('mem '..node.heap())
+    local cooldown = math.random(0, ((cooling * 10) /  #animationData.heat ) + 2);
+    
     -- 1. Cooldown each led
-    print("cooldown "..cooldown)
-    for led = 1, numleds, 1 do
+    for led = 1, #animationData.heat do
       animationData.heat[led] = math.max(0, animationData.heat[led]-cooldown)
     end
 
     -- 2. move each led up and diffuse
-    print("move and diffuse")
-    for led = 3, numleds do
+    for led = 3, #animationData.heat do
       animationData.heat[led] = (animationData.heat[led - 1] + animationData.heat[led - 2] + animationData.heat[led - 2]) / 3;
     end
 
     -- 3. initiate sparks
-    print("sparks")
     if math.random(255) < sparking then
-      local y = math.random(7);
+      local y = math.random(1,8);
       animationData.heat[y] = animationData.heat[y] + math.random(160,255)
     end
 
     -- 4. calculate led colors from heat
-    print("write out")
-    for led = 1, numleds do
-      local t192 = (animationData.heat[led]/255)*191 -- 0-191
-      local heatramp = bit.band(t192, 63) -- 0..63
-      heatramp = bit.lshift(heatramp, 2) -- scale up to 0..252
+    for led = 1, #animationData.heat do
+      local t192 = (animationData.heat[led]/255)*191
+      local heatramp = bit.band(t192, 63)
+      heatramp = bit.lshift(heatramp, 2)
+
+      -- print(led .. " > " .. heatramp)
 
       if heatramp <= 0x40 then
         buffer:set(led, heatramp, 0, 0)
